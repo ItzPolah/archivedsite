b6fc23c6e0571cae888d100026eb330bbf48ee5e
Polar <54783632+ItzPolah@users.noreply.github.com>
2020-07-10 12:37:31 -0400

    added files

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>I'm Polar! &raquo; Community & Social Media Professional</title>
+<meta name="author" content="Brant">
+<link href="assets/css/global.css" rel="stylesheet" media="all"/>
+<script src="https://kit.fontawesome.com/461787f741.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="header">
+        <h1 class="title"> Hi! I'm Polar.</h1>
+        <p class="description">I'm a Community & Social Media Manager, Backend & Frontend Developer, Web & Java Developer.</p> <br/>
+        <p class="blog"> My blog is proudly hosted by BlogSpot! Visit it by clicking <a href="https://polardev.blogspot.com/" class="blog">here.</a></p>
+        <br/><br/><br/>
+        <a href="mailto:itzpolarbear.business@gmail.com?subject=Hey! I want to speak with you regarding..."><i class='fa fa-2x fa-send' title='itzpolarbear.business@gmail.com'></i></a> <a href="http://twitch.tv/itspolarhehe"><i class='fa fa-2x fa-twitch' title='TwitchTV'></i></a> <a href="http://twitter.com/itzpolah"><i class="fa fa-2x fa-twitter" title data-roriginal-title="Twitter"></i></a> <a href="http://github.com/itzpolah"><i class="fa fa-2x fa-github" title data-roriginal-title="GitHub"></i></a> <a href="https://www.reddit.com/user/HeyoItsPolar/"><i class="fa fa-2x fa-reddit" title data-roriginal-title="Reddit"></i></a> 
+    </div>
+    
+</body>
+</html>
+
